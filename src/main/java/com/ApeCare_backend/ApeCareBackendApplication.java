833c7e17b303ce621da071f4271253a221f47d86
Sarah Valdez <s.valdez17@unapec.edu.do>
2025-10-05 15:39:27 -0400

    first commit

---
+++ src/main/java/com/ApeCare_backend/ApeCareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ApeCare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApeCareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApeCareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ApeCare_backend/ApeCareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApeCare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApeCareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
